5c753c3b72ea5bd975ca54b32c5d3abedafbe716
Anjan Rai <rai.unknown@gmail.com>
2022-08-14 13:30:31 +1000

    serial execution of 5 tasks listed

--- index.ts
+++ index.ts
@@ -2,22 +2,14 @@ import { task1 } from './task1';
 import { task2 } from './task2';
 import { task3 } from './task3';
 import { task5 } from './task5';
-import { IindividualData } from './interfaces';
 import { task4 } from './task4';
-const converter = require('json-2-csv');
-const fs = require('fs');
 
-const hello = async (): Promise<void> => {
+const main = async (): Promise<void> => {
   console.log('Hello World');
   await task1();
-  const apiResponse = await task2();
-  const [formattedText, rawData] = task3(apiResponse);
-  task4(rawData);
-};
-hello();
-
-async function runTask5(): Promise<void> {
+  await task2();
+  await task3();
+  await task4();
   await task5();
-}
-
-runTask5();
+};
+main();
