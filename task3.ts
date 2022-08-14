2b6015afe04e14388bcbe0a4c5ee53654c57ff72
Anjan Rai <rai.unknown@gmail.com>
2022-08-14 12:46:36 +1000

    refactoring task3

--- task3.ts
+++ task3.ts
@@ -1,97 +1,41 @@
-interface IResponseData {
-  data: {
-    contentBrowser: {
-      topics: [
-        {
-          units: [
-            {
-              lessons: [
-                {
-                  id: number;
-                  name: string;
-                  subjects: [
-                    {
-                      id: number;
-                      name: string;
-                    }
-                  ];
-                  years: [{ id: number; name: string }];
-                }
-              ];
-            }
-          ];
-        }
-      ];
-    };
-  };
-}
+const { task2 } = require('./task2');
+import {
+  LessonResponse,
+  Content,
+  Lesson,
+  ByYear,
+  BySubject,
+} from './type-definition';
+import { groupByYear, groupBySubject, formatLesson } from './utilities';
 
-interface IindividualData {
-  Year: string;
-  Subject: string;
-  Lesson: string;
-}
+// use deepClone for safety
+// const deepClone = <T>(object: T): T => JSON.parse(JSON.stringify(object));
 
-export const task3 = (dataObj: IResponseData): [string, IindividualData[]] => {
-  let data: IindividualData[] = [];
-  dataObj.data.contentBrowser.topics.forEach((topic) => {
-    topic.units.forEach((unit) => {
-      unit.lessons.forEach((lesson) => {
-        data.push({
-          Lesson: lesson.name.charAt(0).toUpperCase() + lesson.name.slice(1),
-          Subject:
-            lesson.subjects[0].name.charAt(0).toUpperCase() +
-            lesson.subjects[0].name.slice(1),
-          Year: 'Year ' + lesson.years[0].name,
-        });
-      });
-    });
-  });
+const contentToPrintFormat = (content: Content) => {
+  const formatted = content.data.contentBrowser.topics.flatMap((topic) =>
+    topic.units.flatMap((unit) => unit.lessons.flatMap(formatLesson))
+  );
 
-  let copyData = data.map((a) => Object.assign({}, a));
-  // console.log('Copied Data', copyData);
-  // console.log('Data', data);
+  // {lesson[]} -> {[year]: {lesson[]}}
+  const byYear: ByYear = groupByYear(formatted);
 
-  const groupByYear = data.reduce((group: any, datum) => {
-    let year = datum.Year;
-    delete datum.Year;
-    group[year] = group[year] ?? [];
-    group[year].push(datum);
-    return group;
-  }, {});
+  // {[year]: {lesson[]}} -> {[year]: {[subject]: {lesson[]}}}
+  const bySubject: BySubject = groupBySubject(byYear);
 
-  // console.log('Group By Year', groupByYear);
-
-  let formattedData: any = {};
-  for (let k in groupByYear) {
-    let groupBySubject = groupByYear[k].reduce((group: any, datum: any) => {
-      let subject = datum.Subject;
-      delete datum.Subject;
-      group[subject] = group[subject] ?? [];
-      group[subject].push(datum);
-      return group;
-    }, {});
-    formattedData[k] = groupBySubject;
-  }
-
-  // console.log('Formatted Data', JSON.stringify(formattedData, null, 2));
-
-  let formattedText = '';
-  for (let k in formattedData) {
-    formattedText += k;
-    for (let key in formattedData[k]) {
-      formattedText += '\n  ' + key;
-      const lessons = formattedData[k][key].reduce((group, lessonObj) => {
-        group =
-          group instanceof Object
-            ? '\n    ' + group.Lesson + '\n    ' + lessonObj.Lesson
-            : group + '\n    ' + lessonObj.Lesson;
-        return group;
-      });
-      formattedText += lessons;
+  let outputString = '';
+  for (const [year, subjects] of Object.entries(bySubject)) {
+    outputString += year + '\n';
+    for (const [subject, lessons] of Object.entries(subjects)) {
+      outputString += `\t${subject}\n`;
+      for (const lesson of lessons) {
+        outputString += `\t\t${lesson.Lesson}\n`;
+      }
     }
   }
+  return outputString;
+};
 
-  console.log(formattedText);
-  return [formattedText, copyData];
+export const task3 = async () => {
+  const apiResponse = await task2();
+  console.log(contentToPrintFormat(apiResponse));
 };
